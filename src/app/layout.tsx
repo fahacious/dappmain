32a9ad288f7d7775eaef3ac7b3dc27efcd3d5e8e
fahacious <144079390+fahacious@users.noreply.github.com>
2024-09-20 21:32:17 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Mulish({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DApps | Wallet Troubleshooter",
-  description: "DApps | Wallet Troubleshooter",
+  title: "Blocks Mainnet | Wallet Troubleshooter",
+  description: "Blocks Mainnet | Wallet Troubleshooter",
 };
 
 export default function RootLayout({
